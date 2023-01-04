51959e39b61a789549c3bd0447e9e16983555b3d
Maermin <40888772+Maermin@users.noreply.github.com>
2023-01-04 15:29:43 +0100

    Active User LastLogon X days ago.

--- Active User LastLogon X days ago.ps1
+++ Active User LastLogon X days ago.ps1
@@ -4,5 +4,5 @@ Search-ADAccount -Searchbase "OU=YOUR,OU=OU,OU=STRUCTUR,DC=*,DC=*" -AccountInact
 Where {$_.Enabled -eq "True"} |
 #sorted by last logon date
 sort -property LastLogonDate -desc |
-#gives us a .csv file with the names of the users, the last logon tag and whether the user is enabled.
+#gives us a csv file with the names of the users, the last logon tag and whether the user is enabled.
 ft Name, LastLogonDate, Enabled -autosize  | Out-File  C:\lastxdaysoff.csv
