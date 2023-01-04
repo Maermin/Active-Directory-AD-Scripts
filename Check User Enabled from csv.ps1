c5269fd6cf3bccd43bedb911153f668c6c9f6f5f
Maermin <40888772+Maermin@users.noreply.github.com>
2023-01-04 14:48:46 +0100

    Add files via upload
    powershell script to check if a .csv list of users is disabled or enabled

---
+++ Check User Enabled from csv.ps1
@@ -0,0 +1,4 @@
+#This command pulls in a CSV file AD User
+Get-Content C:\Dataowner.csv | Get-ADUser | 
+#Check user is disabled or enabled and is output as a .csv file.
+select SamAccountName,Enabled | Out-File  C:\StatusAccounts.csv
