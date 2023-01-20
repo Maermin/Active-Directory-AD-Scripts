7d4e1fafa37ab5018cfa02c30a220d73ff0c2a54
Maermin <40888772+Maermin@users.noreply.github.com>
2023-01-20 18:35:44 +0100

    Update Copy user groups and apply to others.ps1

--- Copy user groups and apply to others.ps1
+++ Copy user groups and apply to others.ps1
@@ -1,2 +1,2 @@
-#choose your reference user							                   #user who should get the permissions
+#choose your reference user							           #user who should get the permissions
 Get-ADUser -Identity Paul -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members Tobi
